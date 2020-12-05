8b1f0d4e6dc67f17847d82a3738af50323f6defa
Young <yunweiy0211@gmail.com>
2020-12-05 16:12:13 -0500

    change to .php

---
+++ FrontPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Front Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merriweather" />
+</head>
+<body>
+<nav>
+    <a href="FrontPage.php" id="logo"><img src="pics/logo-sided-inverted.png" height="30"></a>
+    <a href="FrontPage.php" class="active">Home</a>
+    <a href="Review.php">Review</a>
+    <a href="MyAccount.php">My account</a>
+    <a href="Search.php">Search</a>
+    <button><a href="Signup.php">Sign up</a></button>
+    <button><a href="Login.php">Log in</a></button>
+</nav>
+<div id="frontPage">
+    <br/>
+    <br/>
+    <div id="introduction">
+        <p>Welcome to Rate My LandLords!</p>
+        <button><a href="Search.php">Search a landlord <span>&#8594;</span></a></button><br/>
+        <button><a href="Review.php">Write a review <span>&#8594;</span></a></button><br/>
+    </div>
+</div>
+<footer>
+    <table>
+        <tr>
+            <td>By Happy Web Programming team(No. 19)</td>
+        </tr>
+        <tr>
+            <td><a href="ContactUs.php">Contact us</a></td>
+        </tr>
+    </table>
+</footer>
+</body>
+</html>
