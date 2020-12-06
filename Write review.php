cba1acbf279643a5b32627b4004066026dabcb88
yifan-He <74215393+yifan-He@users.noreply.github.com>
2020-12-06 13:58:55 -0500

    Update Write review.php

--- Write review.php
+++ Write review.php
@@ -8,59 +8,53 @@ session_start();
     <meta charset =  “utf-8” />
     <link rel="stylesheet" type="text/css" href="style.css">
     <style>
-    div.stars {
-    width: 270px;
-    display: inline-block;
-    }
 
-    input.star { display: none; }
+        div.stars {
+            width: 270px;
+            display: inline-block;
+            text-align: center;
+        }
 
-    label.star {
-    float: right;
-    padding: 10px;
-    font-size: 36px;
-    color: #444;
-    transition: all .2s;
-    }
+        input.star { display: none; }
 
-    input.star:checked ~ label.star:before {
-    content: '\2605';
-    color: #FD4;
-    transition: all .25s;
-    }
+        label.star {
+            float: right;
+            padding: 10px;
+            font-size: 36px;
+            color: #444;
+            transition: all .2s;
+        }
 
-    input.star-5:checked ~ label.star:before {
-    color: #FE7;
-    text-shadow: 0 0 20px #952;
-    }
+        input.star:checked ~ label.star:before {
+            content: '\2605';
+            color: #FD4;
+            transition: all .25s;
+        }
 
-    input.star-1:checked ~ label.star:before { color: #F62; }
+        input.star-5:checked ~ label.star:before {
+            color: #FE7;
+            text-shadow: 0 0 20px #952;
+        }
 
-    label.star:hover { transform: rotate(-15deg) scale(1.3); }
+        input.star-1:checked ~ label.star:before { color: #F62; }
 
-    label.star:before {
-    content: '\2605';
+        label.star:hover { transform: rotate(-15deg) scale(1.3); }
+
+        label.star:before {
+            content: '\2605';
     </style>
 </head>
 <body>
 <nav>
-    <a href="FrontPage.php" id="logo"><img src="pics/logo-sided-inverted.png" height="30"></a>
+    <a href="FrontPage.php" id="logo"><img src="pics/logo-sided-inverted.png" height="30" alt=" loggo"></a>
     <a href="FrontPage.php">Home</a>
-    <a href="Search.php">Search</a>
+    <a href="Review.php" class="active">Review</a>
     <a href="MyAccount.php">My account</a>
-    <?php
-    if(isset($_SESSION["login"])&&$_SESSION["login"]){
-        echo '<button><a href="SignOut.php">Sign out</a></button>';
-        echo "<a href=\"MyAccount.php\" class='user'>{$_SESSION["name"]}, welcome!</a>";
-    }
-    else{
-        echo '<button><a href="Signup.php">Sign up</a></button>';
-        echo '<button><a href="Login.php">Log in</a></button>';
-    }
-    ?>
+    <a href="Search.php">Search</a>
+    <button><a href="Signup.php">Sign up</a></button>
+    <button><a href="Login.php">Log in</a></button>
 </nav>
 <main id="write_review">
-    <h1>How many starts are you rating for</h1>
     <div class="stars">
         <form action="">
             <input class="star star-5" id="star-5" type="radio" name="star"/>
@@ -73,8 +67,19 @@ session_start();
             <label class="star star-2" for="star-2"></label>
             <input class="star star-1" id="star-1" type="radio" name="star"/>
             <label class="star star-1" for="star-1"></label>
+            <br/>
+            <label>How many starts are you rating for</label>
+            <br/><br/>
+            <label>What is your comment?</label>
+            <input type = "text"/>
+            <br/> <br/>
+            <label>Thanks for your appreciation!</label>
+            <br/> <br/>
+            <input type = "submit" value = "submit"/>
+            <input type = "reset" value = "start over"/>
         </form>
     </div>
+
 
 
 </main>
